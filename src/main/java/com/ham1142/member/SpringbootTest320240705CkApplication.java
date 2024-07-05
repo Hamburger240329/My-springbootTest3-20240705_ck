dc87d644cea665a6cd6e402b0b70b81ef02a3eb9
JBEDU29 <5514cyj@gmail.com>
2024-07-05 15:58:05 +0900

    환경설정 및 회원가입 기능 추가 + 테스트 기능 추가

---
+++ src/main/java/com/ham1142/member/SpringbootTest320240705CkApplication.java
@@ -0,0 +1,13 @@
+package com.ham1142.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest320240705CkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTest320240705CkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ham1142/member/SpringbootTest320240705CkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ham1142.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest320240705CkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
